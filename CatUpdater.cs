b5f7be319664678a12d12865ecfe6104b78e50cf
dtytomi <dtytomi@gmail.com>
2016-04-08 16:17:34 +0100

    Zombie Conga Unity 3D

---
+++ CatUpdater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatUpdater : MonoBehaviour {
+
+	private CatController catController;
+
+	// Use this for initialization
+	void Start () {
+		catController = transform.parent.GetComponent<CatController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		catController.UpdateTargetPosition();
+	}
+
+	void OnBecameInvisible() {
+		catController.OnBecameInvisible();
+	}
+
+	void GrantCatTheSweetReleaseOfDeath(){
+
+		Destroy (transform.parent.gameObject);
+	}
+}
---
+++ KittenCreator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KittenCreator : MonoBehaviour {
+
+	public float minSpawnTime = 0.75f;
+	public float maxSpawnTime = 2f;
+	public GameObject catPrefab;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("SpawnCat", minSpawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void SpawnCat() {
+
+		Camera camera = Camera.main;
+		Vector3 cameraPos = camera.transform.position;
+		float xMax = camera.aspect * camera.orthographicSize;
+		float xRange = camera.aspect * camera.orthographicSize * 1.75f;
+		float yMax = camera.orthographicSize - 0.5f;
+
+		Vector3 catPos = 
+			new Vector3 (cameraPos.x + Random.Range (xMax - xRange, yMax),
+			            Random.Range (-yMax, yMax),
+			            catPrefab.transform.position.z);
+
+		Instantiate(catPrefab, catPos, Quaternion.identity);
+
+		Invoke ("SpawnCat", Random.Range(minSpawnTime, maxSpawnTime));
+	}
+}
